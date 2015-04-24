f3479fbb3ccae91098401d77f1333b7d97d975f5
Callie Brooks <calidesigns42@gmail.com>
2015-04-24 06:52:10 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
-var canvas = document.getElementById('webHdr');
-var ctx = canvas.getContext('2d');
+function draw() {
+    'use strict';
+    var canvas = document.getElementById("webHdr");
+    if (canvas.getContext) {
+        var ctx = canvas.getContext('2d');
+    }
+}
